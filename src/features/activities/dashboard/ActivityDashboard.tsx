5a093a7583e0e8882c06b65a4275f19f294a8504
frankaddelia <faddelia@gmail.com>
2022-07-13 13:00:52 -0500

    Add and use activitydetails component

--- src/features/activities/dashboard/ActivityDashboard.tsx
+++ src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,6 +1,7 @@
 import react from 'react';
 import { Grid, List } from 'semantic-ui-react';
 import { Activity } from '../../../app/models/activity';
+import ActivityDetails from '../details/ActivityDetails';
 import ActivityList from './ActivityList';
 
 interface Props {
@@ -8,11 +9,16 @@ interface Props {
 }
 
 export default function ActivityDashboard({activities}: Props) {
-    return (
-      <Grid>
-        <Grid.Column width="10">
-          <ActivityList activities={activities} />
+  return (
+    <Grid>
+      <Grid.Column width="10">
+        <ActivityList activities={activities} />
+      </Grid.Column>
+        <Grid.Column width="6">
+          {activities[0] && 
+          <ActivityDetails activity={activities[0]} />
+          }
         </Grid.Column>
-      </Grid>
-    )
+    </Grid>
+  )
 }
