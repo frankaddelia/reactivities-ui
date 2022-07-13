34a27d6b583c72d034bd7d286a172d7936c639d4
frankaddelia <faddelia@gmail.com>
2022-07-13 17:33:47 -0500

    Add edit mode state

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -8,6 +8,7 @@ import ActivityDashboard from '../../features/activities/dashboard/ActivityDashb
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
   const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
+  const [editMode, setEditMode] = useState(false);
 
   useEffect(() => {
     axios.get<Activity[]>('http://localhost:5000/api/activities').then(response => {
