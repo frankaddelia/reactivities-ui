0fcc8598fe3ccf7fe23820c7fd291ac1d455476c
Frank <faddelia@gmail.com>
2022-07-13 07:14:40 -0500

    Use two space formatting

--- src/app/layout/NavBar.tsx
+++ src/app/layout/NavBar.tsx
@@ -2,18 +2,18 @@ import React from 'react';
 import { Button, Container, Menu } from 'semantic-ui-react';
 
 export default function NavBar() {
-    return (
-        <Menu inverted fixed="top">
-            <Container>
-                <Menu.Item header>
-                    <img src="/assets/logo.png" alt="logo" style={{marginRight: '10px'}} />
-                    Reactivities
-                </Menu.Item>
-                <Menu.Item name="activities" />
-                <Menu.Item>
-                    <Button positive content="Create Activity" />
-                </Menu.Item>
-            </Container>
-        </Menu>
-    )
+  return (
+    <Menu inverted fixed="top">
+      <Container>
+        <Menu.Item header>
+          <img src="/assets/logo.png" alt="logo" style={{ marginRight: '10px' }} />
+          Reactivities
+        </Menu.Item>
+        <Menu.Item name="activities" />
+        <Menu.Item>
+          <Button positive content="Create Activity" />
+        </Menu.Item>
+      </Container>
+    </Menu>
+  )
 }
