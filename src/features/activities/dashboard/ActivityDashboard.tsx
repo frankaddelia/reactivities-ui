92f325a641b2bc187f49acc18593d47a2d9fe8c9
Frank <faddelia@gmail.com>
2022-07-13 18:27:59 -0500

    Delete Activity
    Add delete activity functionality

--- src/features/activities/dashboard/ActivityDashboard.tsx
+++ src/features/activities/dashboard/ActivityDashboard.tsx
@@ -14,6 +14,7 @@ interface Props {
     openForm: (id: string) => void;
     closeForm: () => void;
     createOrEdit: (activity: Activity) => void;
+    deleteActivity: (id: string) => void;
 }
 
 export default function ActivityDashboard({
@@ -24,7 +25,8 @@ export default function ActivityDashboard({
   editMode,
   openForm,
   closeForm,
-  createOrEdit
+  createOrEdit,
+  deleteActivity
 }: Props) {
   return (
     <Grid>
@@ -32,6 +34,7 @@ export default function ActivityDashboard({
         <ActivityList
           activities={activities}
           selectActivity={selectActivity}
+          deleteActivity={deleteActivity}
         />
       </Grid.Column>
         <Grid.Column width="6">
--- src/features/activities/dashboard/ActivityList.tsx
+++ src/features/activities/dashboard/ActivityList.tsx
@@ -5,9 +5,10 @@ import { Activity } from '../../../app/models/activity';
 interface Props {
   activities: Activity[];
   selectActivity: (id: string) => void;
+  deleteActivity: (id: string) => void;
 }
 
-export default function ActivityList({ activities, selectActivity }: Props) {
+export default function ActivityList({ activities, selectActivity, deleteActivity }: Props) {
   return (
     <Segment>
       <Item.Group divided>
@@ -30,6 +31,12 @@ export default function ActivityList({ activities, selectActivity }: Props) {
                   content="View"
                   color="blue"
                   onClick={() => selectActivity(activity.id)}
+                />
+                <Button
+                  floated="right"
+                  content="Delete"
+                  color="red"
+                  onClick={() => deleteActivity(activity.id)}
                 />
                 <Label content={activity.category} />
               </Item.Extra>
