4b56486589974cbb8dbdb28bee801f7737d25517
faddelia <faddelia@gmail.com>
2022-07-13 18:32:44 -0500

    Front end activity CRUD operations (#1)
    * Add activity interface. Use activity interface
    
    * Add images to assets folder. Add navbar component
    
    * Use navbar component. Use semantic components
    
    * Add styles
    
    * Add activity dashboard
    
    * Use activity dashboard component
    
    * Use two space formatting
    
    * Add activity list component
    
    * Use activity list component
    
    * Add and use activitydetails component
    
    * Add activity form
    
    * Add functionality to show the currently selected activity in hte activity details component
    
    * Add edit mode state
    
    * Add functions to handle form open and close
    
    * Add function properties
    
    Add editMode, openForm, and closeForm properties
    
    * Accept new properties
    
    Update the interface and parameters
    
    * New property for navbar
    
    Pass new prop down to navbar
    
    * New properties
    
    Add new properties and pass them down to child components
    
    * Hide details
    
    Hide details when editing
    
    * Close form
    
    Add click event to close form
    
    * Manage form state
    
    Manage the form state for input
    
    * Add create or edit function
    
    * Clean up
    
    Clean up unused imports. Install UUID
    
    * Use UUID
    
    Use UUID when creating a new activity
    
    * Delete Activity
    
    Add delete activity functionality

---
+++ src/features/activities/form/ActivityForm.tsx
@@ -0,0 +1,79 @@
+import React, { ChangeEvent, useState } from 'react';
+import { Button, Form, Segment } from 'semantic-ui-react';
+import { Activity } from '../../../app/models/activity';
+
+interface Props {
+  activity: Activity | undefined;
+  closeForm: () => void;
+  createOrEdit: (activity: Activity) => void;
+}
+
+export default function ActivityForm({activity: selectedActivity, closeForm, createOrEdit}: Props) {
+  const initialState = selectedActivity ?? {
+    id: '',
+    title: '',
+    category: '',
+    description: '',
+    date: '',
+    city: '',
+    venue: '',
+  }
+
+  const [activity, setActivity] = useState(initialState);
+
+  function handleSubmit() {
+    createOrEdit(activity);
+  }
+
+  function handleInputChange(e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
+    const { name, value } = e.target;
+    setActivity({
+      ...activity,
+      [name]: value
+    })
+  }
+
+  return (
+    <Segment clearing>
+      <Form onSubmit={handleSubmit} autoComplete="off">
+        <Form.Input
+          name="title"
+          value={activity.title}
+          placeholder="Title"
+          onChange={handleInputChange}
+        />
+        <Form.TextArea placeholder="Description"
+          name="description"
+          value={activity.description}
+          onChange={handleInputChange}
+        />
+        <Form.Input
+          name="category"
+          value={activity.category}
+          placeholder="Category"
+          onChange={handleInputChange}
+        />
+        <Form.Input
+          name="date"
+          value={activity.date}
+          placeholder="Date"
+          onChange={handleInputChange}
+        />
+        <Form.Input
+          name="city"
+          value={activity.city}
+          placeholder="City"
+          onChange={handleInputChange}
+        />
+        <Form.Input
+          name="venue"
+          value={activity.venue}
+          placeholder="Venue"
+          onChange={handleInputChange}
+        />
+        <Button floated="right" positive type="submit" content="Submit" onChange={handleInputChange} />
+        <Button floated="right" type="button" content="Cancel" onClick={closeForm} onChange={handleInputChange} />
+      </Form>
+    </Segment>
+  )
+}
