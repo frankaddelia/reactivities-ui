192f7d7d738a605ca6bf7690283750d2c3f430ad
Frank <faddelia@gmail.com>
2022-07-14 06:40:03 -0500

    Add submitting flag to edit and create buttons so they can't be double clicked

--- src/features/activities/form/ActivityForm.tsx
+++ src/features/activities/form/ActivityForm.tsx
@@ -6,9 +6,10 @@ interface Props {
   activity: Activity | undefined;
   closeForm: () => void;
   createOrEdit: (activity: Activity) => void;
+  submitting: boolean;
 }
 
-export default function ActivityForm({activity: selectedActivity, closeForm, createOrEdit}: Props) {
+export default function ActivityForm({activity: selectedActivity, closeForm, createOrEdit, submitting}: Props) {
   const initialState = selectedActivity ?? {
     id: '',
     title: '',
@@ -72,7 +73,7 @@ export default function ActivityForm({activity: selectedActivity, closeForm, cre
           placeholder="Venue"
           onChange={handleInputChange}
         />
-        <Button floated="right" positive type="submit" content="Submit" onChange={handleInputChange} />
+        <Button floated="right" positive type="submit" loading={submitting} content="Submit" onChange={handleInputChange} />
         <Button floated="right" type="button" content="Cancel" onClick={closeForm} onChange={handleInputChange} />
       </Form>
     </Segment>
