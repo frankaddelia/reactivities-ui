c44dad75d9b3f7c2a910ca9cfd73d63e5d72eda6
Frank <faddelia@gmail.com>
2022-07-14 07:37:30 -0500

    Install MobX. Add new store

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -6,8 +6,11 @@ import ActivityDashboard from '../../features/activities/dashboard/ActivityDashb
 import { v4 as uuid } from 'uuid';
 import agent from '../api/agent';
 import LoadingComponents from './LoadingComponents';
+import { useStore } from '../stores/Store';
 
 function App() {
+  const { activityStore } = useStore();
+
   const [activities, setActivities] = useState<Activity[]>([]);
   const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
   const [editMode, setEditMode] = useState(false);
@@ -82,6 +85,8 @@ function App() {
     <>
       <NavBar openForm={handleFormOpen} />
         <Container style={{marginTop: '7em'}}>
+          <h2>{activityStore.title}</h2>
+
           <ActivityDashboard
             activities={activities}
             selectedActivity={selectedActivity}
