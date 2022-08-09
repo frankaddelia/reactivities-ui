57e750fbd871c20f2a249189f61677e414fe5fce
Frank <faddelia@gmail.com>
2022-08-08 20:51:31 -0500

    Error testing
    Install react toastify. Add test error component to test bad server responses

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -7,12 +7,15 @@ import { Route, Routes, useLocation } from 'react-router-dom';
 import HomePage from '../../features/activities/home/HomePage';
 import ActivityForm from '../../features/activities/form/ActivityForm';
 import ActivityDetails from '../../features/activities/details/ActivityDetails';
+import TestErrors from '../../features/errors/TestError';
+import { ToastContainer } from 'react-toastify';
 
 function App() {
   const location = useLocation();
 
   return (
     <>
+      <ToastContainer position="bottom-right" hideProgressBar />
       <Routes>
         <Route path="/" element={<HomePage />} />
       </Routes>
@@ -23,6 +26,7 @@ function App() {
           <Route path="/activities/:id" element={<ActivityDetails />} />
           <Route key={location.key} path="/createActivity" element={<ActivityForm />} />
           <Route key={location.key} path="/manage/:id" element={<ActivityForm />} />
+          <Route path="/errors" element={<TestErrors />} />
         </Routes>
       </Container>
     </>
