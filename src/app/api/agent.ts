b627d1ec672a680263f626823bde0564b94107b4
Frank <faddelia@gmail.com>
2022-08-19 16:32:26 -0500

    Handle validation errors and bad request

--- src/app/api/agent.ts
+++ src/app/api/agent.ts
@@ -15,10 +15,24 @@ axios.interceptors.response.use(async response => {
   await sleep(1000);
   return response;
 }, (error: AxiosError) => {
-  const {data, status} = error.response!;
+  const {data:d, status} = error.response!;
+  let data: any = d!;
+
   switch (status) {
     case 400:
-      toast.error('Bad request');
+      if (data.errors) {
+        const modalStateErrors = [];
+
+        for (const key in data.errors) {
+          if (data.errors[key]) {
+            modalStateErrors.push(data.errors[key]);
+          }
+        }
+
+        throw modalStateErrors.flat();
+      } else {
+        toast.error(data);
+      }
       break;
     case 401: 
       toast.error("Unauthorized");
