a29d457bd746919b23006664f91d42c9286b6832
Frank <faddelia@gmail.com>
2022-09-02 16:06:49 -0500

    Pass data to server error template, add route

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -10,6 +10,7 @@ import ActivityDetails from '../../features/activities/details/ActivityDetails';
 import TestErrors from '../../features/errors/TestError';
 import { ToastContainer } from 'react-toastify';
 import NotFound from '../../features/errors/NotFound';
+import ServerError from '../../features/errors/ServerError';
 
 function App() {
   const location = useLocation();
@@ -28,6 +29,7 @@ function App() {
           <Route key={location.key} path="/createActivity" element={<ActivityForm />} />
           <Route key={location.key} path="/manage/:id" element={<ActivityForm />} />
           <Route path="/errors" element={<TestErrors />} />
+          <Route path="/server-error" element={<ServerError />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </Container>
