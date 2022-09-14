5dbbfe35404593602f09f9aaf9b26d53401f680a
Frank <faddelia@gmail.com>
2022-09-14 16:14:25 -0500

    Refactor to use formik and yup validation

--- src/features/activities/form/ActivityForm.tsx
+++ src/features/activities/form/ActivityForm.tsx
@@ -2,12 +2,13 @@ import { observer } from 'mobx-react-lite';
 import { loadavg } from 'os';
 import React, { ChangeEvent, useEffect, useState } from 'react';
 import { Link, useNavigate, useParams } from 'react-router-dom';
-import { Button, FormField, Label, Segment } from 'semantic-ui-react';
+import { Button, Label, Segment } from 'semantic-ui-react';
 import LoadingComponents from '../../../app/layout/LoadingComponents';
 import { useStore } from '../../../app/stores/Store';
 import { v4 as uuid } from 'uuid';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
+import MyTextInput from '../../../app/common/form/MyTextInput';
 
 export default observer (function ActivityForm() {
   const navigate = useNavigate();
@@ -33,7 +34,12 @@ export default observer (function ActivityForm() {
   });
 
   const validationSchema = Yup.object({
-    title: Yup.string().required('The activity title is required')
+    title: Yup.string().required('The activity title is required'),
+    description: Yup.string().required('The activity description is required'),
+    category: Yup.string().required(),
+    date: Yup.string().required(),
+    venue: Yup.string().required(),
+    city: Yup.string().required(),
   });
 
   useEffect(() => {
@@ -77,39 +83,12 @@ export default observer (function ActivityForm() {
       >
         {({handleSubmit}) => (
           <Form className="ui form" onSubmit={handleSubmit} autoComplete="off">
-            <FormField>
-              <Field
-                name="title"
-                placeholder="Title"
-              />
-              <ErrorMessage
-                name="title"
-                render={error => 
-                  <Label basic color="red" content={error} />
-                } 
-              />
-            </FormField>
-            <Field
-              placeholder="Description"
-              name="description"
-            />
-            <Field
-              name="category"
-              placeholder="Category"
-            />
-            <Field
-              type="date"
-              name="date"
-              placeholder="Date"
-            />
-            <Field
-              name="city"
-              placeholder="City"
-            />
-            <Field
-              name="venue"
-              placeholder="Venue"
-            />
+            <MyTextInput name="title" placeholder="Title" />
+            <MyTextInput placeholder="Description" name="description" />
+            <MyTextInput placeholder="Category" name="category" />
+            <MyTextInput placeholder="Date" name="date" />
+            <MyTextInput placeholder="City" name="city" />
+            <MyTextInput placeholder="Venue" name="venue" />
             <Button floated="right" positive type="submit" loading={loading} content="Submit" />
             <Button as={Link} to="/activities" floated="right" type="button" content="Cancel" />
           </Form>
