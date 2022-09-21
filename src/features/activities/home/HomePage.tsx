c1bcfb7e968404b457fa93cea221ccbf9b6d223b
Frank <faddelia@gmail.com>
2022-09-21 16:58:15 -0500

    Add reactivities title next to logo

--- src/features/activities/home/HomePage.tsx
+++ src/features/activities/home/HomePage.tsx
@@ -13,6 +13,7 @@ export default observer(function HomePage() {
       <Container text>
         <Header as="h1" inverted>
           <Image size="massive" src="/assets/logo.png" alt="logo" style={{marginBottom: 12}} />
+          Reactivities
         </Header>
         {userStore.isLoggedIn ? (
           <>
