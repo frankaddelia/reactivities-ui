589387c423538b28c15f85c850ef5293ae89a1bb
Frank <faddelia@gmail.com>
2022-09-21 11:27:09 -0500

    Add login functionality

--- src/app/stores/userStore.ts
+++ src/app/stores/userStore.ts
@@ -1,7 +1,9 @@
-import { makeAutoObservable } from "mobx";
+import { makeAutoObservable, runInAction } from "mobx";
 import { isArgumentsObject } from "util/types";
+import { history } from "../..";
 import agent from "../api/agent";
 import { User, UserFormValues } from "../models/user";
+import { store } from "./Store";
 
 export default class UserStore {
   user: User | null = null;
@@ -17,9 +19,18 @@ export default class UserStore {
   login = async (creds: UserFormValues) => {
     try {
       const user = await agent.Account.login(creds);
-      console.log(user);
+      store.commonStore.setToken(user.token);
+      runInAction(() => this.user = user);
+      history.push('/activities');
     } catch (error) {
       throw error;
     }
+  }
+
+  logout = () => {
+    store.commonStore.setToken(null);
+    window.localStorage.removeItem('jwt');
+    this.user = null;
+    history.push('/');
   }
 }
