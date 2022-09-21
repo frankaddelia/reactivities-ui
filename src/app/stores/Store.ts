9f028e5cc19433effbb3a4ce5e023d3abd4e89c5
Frank <faddelia@gmail.com>
2022-09-21 16:51:35 -0500

    Add modal store to the store

--- src/app/stores/Store.ts
+++ src/app/stores/Store.ts
@@ -1,17 +1,20 @@
 import { createContext, useContext } from "react";
 import ActivityStore from "./ActivityStore";
 import CommonStore from "./commonStore";
+import ModalStore from "./modalStore";
 import UserStore from "./userStore";
 
 interface Store {
   activityStore: ActivityStore;
   commonStore: CommonStore;
+  modalStore: ModalStore;
   userStore: UserStore;
 }
 
 export const store: Store = {
   activityStore: new ActivityStore(),
   commonStore: new CommonStore(),
+  modalStore: new ModalStore(),
   userStore: new UserStore()
 }
 
