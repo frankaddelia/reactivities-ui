bc405d3b2b8fe01b6e05fa8ffdced5713a394ca9
Frank <faddelia@gmail.com>
2022-09-22 09:04:50 -0500

    Add register form

---
+++ src/features/users/RegisterForm.tsx
@@ -0,0 +1,55 @@
+import { ErrorMessage, Form, Formik } from 'formik';
+import { observer } from 'mobx-react-lite';
+import React from 'react';
+import { Button, Header, Label } from 'semantic-ui-react';
+import MyTextInput from '../../app/common/form/MyTextInput';
+import { useStore } from '../../app/stores/Store';
+import * as Yup from 'yup';
+
+export default observer (function RegisterForm() {
+  const {userStore} = useStore();
+
+  return(
+    <Formik
+      initialValues={{displayName: '', username: '', email: '', password: '', error: null}}
+      onSubmit={(values, {setErrors}) =>
+        userStore.register(values)
+        .catch(error => setErrors({error: 'Invalid email or password.'}))}
+      validationSchema={Yup.object({
+        displayName: Yup.string().required(),
+        username: Yup.string().required(),
+        email: Yup.string().required().email(),
+        password: Yup.string().required(),
+      })}
+    >
+      {({handleSubmit, isSubmitting, errors, isValid, dirty}) => (
+        <Form className='ui form' onSubmit={handleSubmit} autoComplete='off'>
+          <Header as='h2' content='Sign up to Reactivities' color='teal' textAlign='center' />
+          <MyTextInput name='displayName' placeholder='Display Name' />
+          <MyTextInput name='username' placeholder='Username' />
+          <MyTextInput name='email' placeholder='Email' />
+          <MyTextInput type='password' name='password' placeholder='Password' />
+          <ErrorMessage
+            name='error'
+            render={() =>
+              <Label
+                style={{marginBottom: 10}}
+                basic
+                color='red'
+                content={errors.error}
+              />
+            }
+          />
+          <Button
+            positive
+            content='Register'
+            type='submit'
+            fluid
+            loading={isSubmitting}
+            disabled={!isValid || !dirty || isSubmitting}
+          />
+        </Form>
+      )}
+    </Formik>
+  )
+});
