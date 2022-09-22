0009d416bd1d467747bfe5864a1eeed49faad28c
Frank <faddelia@gmail.com>
2022-09-22 09:28:00 -0500

    Add register form validation. Display errors properly

--- src/features/users/RegisterForm.tsx
+++ src/features/users/RegisterForm.tsx
@@ -1,10 +1,11 @@
 import { ErrorMessage, Form, Formik } from 'formik';
 import { observer } from 'mobx-react-lite';
 import React from 'react';
-import { Button, Header, Label } from 'semantic-ui-react';
+import { Button, Header } from 'semantic-ui-react';
 import MyTextInput from '../../app/common/form/MyTextInput';
 import { useStore } from '../../app/stores/Store';
 import * as Yup from 'yup';
+import ValidationErrors from '../errors/ValidationErrors';
 
 export default observer (function RegisterForm() {
   const {userStore} = useStore();
@@ -14,7 +15,7 @@ export default observer (function RegisterForm() {
       initialValues={{displayName: '', username: '', email: '', password: '', error: null}}
       onSubmit={(values, {setErrors}) =>
         userStore.register(values)
-        .catch(error => setErrors({error: 'Invalid email or password.'}))}
+        .catch(error => setErrors({error}))}
       validationSchema={Yup.object({
         displayName: Yup.string().required(),
         username: Yup.string().required(),
@@ -23,7 +24,7 @@ export default observer (function RegisterForm() {
       })}
     >
       {({handleSubmit, isSubmitting, errors, isValid, dirty}) => (
-        <Form className='ui form' onSubmit={handleSubmit} autoComplete='off'>
+        <Form className='ui form error' onSubmit={handleSubmit} autoComplete='off'>
           <Header as='h2' content='Sign up to Reactivities' color='teal' textAlign='center' />
           <MyTextInput name='displayName' placeholder='Display Name' />
           <MyTextInput name='username' placeholder='Username' />
@@ -32,12 +33,7 @@ export default observer (function RegisterForm() {
           <ErrorMessage
             name='error'
             render={() =>
-              <Label
-                style={{marginBottom: 10}}
-                basic
-                color='red'
-                content={errors.error}
-              />
+              <ValidationErrors errors={errors.error} />
             }
           />
           <Button
