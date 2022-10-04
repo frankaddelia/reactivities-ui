c75f2040e2e206de82099c7dcbd205de47b2aa29
Frank <faddelia@gmail.com>
2022-10-04 14:44:48 -0500

    Use the cropper component

--- src/app/common/imageUpload/PhotoUploadWidget.tsx
+++ src/app/common/imageUpload/PhotoUploadWidget.tsx
@@ -1,7 +1,24 @@
-import { Grid, Header } from "semantic-ui-react";
+import { useEffect, useState } from "react";
+import { Grid, Header, Image } from "semantic-ui-react";
+import PhotoWidgetCropper from "./PhotoWidgetCropper";
 import PhotoWidgetDropzone from "./PhotoWidgetDropzone";
 
 export default function PhotoUploadWidget() {
+  const [files, setFiles] = useState<any>([]);
+  const [cropper, setCropper] = useState<Cropper>();
+  
+  function onCrop() {
+    if (cropper) {
+      cropper.getCroppedCanvas().toBlob(blob => console.log(blob));
+    }
+  }
+
+  useEffect(() => {
+    return () => {
+      files.forEach((file: any) => URL.revokeObjectURL(file.preview))
+    }
+  }, [files]);
+
   return (
     <Grid>
       <Grid.Column width={4}>
@@ -10,7 +27,7 @@ export default function PhotoUploadWidget() {
           color='teal'
           content='Step 1 - Add Photo'
         />
-        <PhotoWidgetDropzone />
+        <PhotoWidgetDropzone setFiles={setFiles} />
       </Grid.Column>
       <Grid.Column width={1} />
       <Grid.Column width={4}>
@@ -19,6 +36,12 @@ export default function PhotoUploadWidget() {
           color='teal'
           content='Step 2 - Resize Image'
         />
+        {files && files.length > 0 && (
+          <PhotoWidgetCropper
+            setCropper={setCropper}
+            imagePreview={files[0].preview}
+          />
+        )}
       </Grid.Column>
       <Grid.Column width={1} />
       <Grid.Column width={4}>
