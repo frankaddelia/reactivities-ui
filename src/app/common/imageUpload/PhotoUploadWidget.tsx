29dc7c1ce4b1615c31c276574d7f6a56fb042fd4
Frank <faddelia@gmail.com>
2022-10-04 14:14:22 -0500

    Use photo widget dropzone component

--- src/app/common/imageUpload/PhotoUploadWidget.tsx
+++ src/app/common/imageUpload/PhotoUploadWidget.tsx
@@ -1,4 +1,5 @@
 import { Grid, Header } from "semantic-ui-react";
+import PhotoWidgetDropzone from "./PhotoWidgetDropzone";
 
 export default function PhotoUploadWidget() {
   return (
@@ -9,6 +10,7 @@ export default function PhotoUploadWidget() {
           color='teal'
           content='Step 1 - Add Photo'
         />
+        <PhotoWidgetDropzone />
       </Grid.Column>
       <Grid.Column width={1} />
       <Grid.Column width={4}>
