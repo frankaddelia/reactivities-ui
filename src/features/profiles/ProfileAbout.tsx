67df2f2bd524a9863be8aefa8fd1579d203e9c29
Frank <faddelia@gmail.com>
2022-10-06 13:48:14 -0500

    Use the profile registry

--- src/features/profiles/ProfileAbout.tsx
+++ src/features/profiles/ProfileAbout.tsx
@@ -2,30 +2,27 @@ import { observer } from "mobx-react-lite";
 import { useEffect, useState } from "react";
 import { Button, Grid, Header, Tab } from "semantic-ui-react";
 import { Profile } from "../../app/models/profile";
-import profileStore from "../../app/stores/profileStore";
 import { useStore } from "../../app/stores/Store";
 import ProfileEditForm from "./form/ProfileEditForm";
 
-interface Props {
-  profile: Profile;
-}
-
-export default observer(function ProfileAbout({ profile }: Props) {
+export default observer(function ProfileAbout() {
   const { profileStore } = useStore();
   const { profileRegistry } = profileStore;
   const [isEditing, setIsEditing] = useState<boolean>(false);
-  const [currentProfile, setCurrentProfile] = useState<Profile>(profile);
+  const [profile, setProfile] = useState<Profile | null>(profileStore.profile);
 
   function toggleEditing() {
     setIsEditing(!isEditing);
   }
 
   useEffect(() => {
-    const currentUser = profileRegistry.get(profile.username);
-    if (profile && currentUser) {
-      setCurrentProfile(currentUser);
+    if (profile?.username) {
+      const currentUser = profileRegistry.get(profile.username);
+      if (currentUser) {
+        setProfile(currentUser);
+      }
     }
-  }, [profile, profile.username, profileRegistry]);
+  }, [profileRegistry]);
 
   return (
     <Tab.Pane>
@@ -34,7 +31,7 @@ export default observer(function ProfileAbout({ profile }: Props) {
           <Header
             floated="left"
             icon="user"
-            content={`About ${profile.displayName}`}
+            content={`About ${profile?.displayName}`}
           />
           <Button
             floated="right"
@@ -45,14 +42,14 @@ export default observer(function ProfileAbout({ profile }: Props) {
             {isEditing? "Cancel" : "Edit Profile"}
           </Button>
         </Grid.Column>
-        {isEditing && (
+        {isEditing && profile && (
           <Grid.Column width={16}>
             <ProfileEditForm profile={profile} />
           </Grid.Column>
         )}
         {!isEditing && (
           <p style={{marginBottom: '15px', whiteSpace: 'pre-wrap'}}>
-            {currentProfile.bio}
+            {profile?.bio}
           </p>
         )}
       </Grid>
