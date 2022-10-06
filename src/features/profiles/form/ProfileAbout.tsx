164022b915e6b8af128fd7c36aecf753eb3ca567
Frank <faddelia@gmail.com>
2022-10-06 10:49:58 -0500

    Add profile about component

---
+++ src/features/profiles/form/ProfileAbout.tsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import { Button, Grid, Header, Tab } from "semantic-ui-react";
+import { Profile } from "../../../app/models/profile";
+import ProfileEditForm from "./ProfileEditForm";
+
+interface Props {
+  profile: Profile;
+}
+
+export default function ProfileAbout({ profile }: Props) {
+  const [isEditing, setIsEditing] = useState<boolean>(false);
+
+  function toggleEditing() {
+    setIsEditing(!isEditing);
+  }
+
+  return (
+    <Tab.Pane>
+      <Grid>
+        <Grid.Column width={16}>
+          <Header
+            floated="left"
+            icon="user"
+            content={`About ${profile.displayName}`}
+          />
+          <Button
+            floated="right"
+            primary={!isEditing}
+            basic
+            onClick={toggleEditing}
+          >
+            {isEditing? "Cancel" : "Edit Profile"}
+          </Button>
+        </Grid.Column>
+        {isEditing && (
+          <Grid.Column width={16}>
+            <ProfileEditForm profile={profile} />
+          </Grid.Column>
+        )}
+        {!isEditing && (
+          <Tab.Pane>
+            {profile.bio}
+          </Tab.Pane>
+        )}
+      </Grid>
+    </Tab.Pane>
+  );
+}
