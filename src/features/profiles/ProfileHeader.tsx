f987fd3dc219d573011d75ae8a21bdf5af418cf2
Frank <faddelia@gmail.com>
2022-10-14 11:55:08 -0500

    Use values for followers and following

--- src/features/profiles/ProfileHeader.tsx
+++ src/features/profiles/ProfileHeader.tsx
@@ -22,8 +22,8 @@ export default observer(function ProfileHeader({profile}: Props) {
         </Grid.Column>
         <Grid.Column width={4}>
           <Statistic.Group widths={2}>
-            <Statistic label='Followers' value="5" />
-            <Statistic label='Followering' value="42" />
+            <Statistic label='Followers' value={profile.followersCount} />
+            <Statistic label='Followering' value={profile.followingCount} />
           </Statistic.Group>
           <Divider />
           <Reveal animated='move'>
