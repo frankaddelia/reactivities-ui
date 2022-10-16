22f257787b71fd00db2378427ce294de8f952fd9
Frank <faddelia@gmail.com>
2022-10-16 07:23:09 -0500

    Add follower and following logic

--- src/features/profiles/ProfileContent.tsx
+++ src/features/profiles/ProfileContent.tsx
@@ -1,6 +1,7 @@
 import { observer } from "mobx-react-lite";
 import { Tab } from "semantic-ui-react";
 import { Profile } from "../../app/models/profile";
+import { useStore } from "../../app/stores/Store";
 import ProfileAbout from "./ProfileAbout";
 import ProfileFollowings from "./ProfileFollowings";
 import ProfilePhotos from "./ProfilePhotos";
@@ -10,6 +11,7 @@ interface Props {
 }
 
 export default observer(function ProfileContent({profile}: Props) {
+  const { profileStore } = useStore();
   const panes = [
     {menuItem: 'About', render: () => <ProfileAbout />},
     {menuItem: 'Photos', render: () => <ProfilePhotos profile={profile} />},
@@ -23,6 +25,7 @@ export default observer(function ProfileContent({profile}: Props) {
       menu={{fluid: true, vertical: true}}
       menuPosition='right'
       panes={panes}
+      onTabChange={(e, data) => profileStore.setActiveTab(data.activeIndex)}
     />
-  )
-})
+  );
+});
