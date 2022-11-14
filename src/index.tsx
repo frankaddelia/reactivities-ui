1e25ad4b8e8961acd273c90f47b4c8d59f502d85
Frank <faddelia@gmail.com>
2022-11-14 17:29:14 -0600

    Add fix for scroll to top

--- src/index.tsx
+++ src/index.tsx
@@ -9,6 +9,7 @@ import reportWebVitals from './reportWebVitals';
 import { store, StoreContext } from './app/stores/Store';
 import { unstable_HistoryRouter as HistoryRouter } from 'react-router-dom';
 import {createBrowserHistory} from 'history';
+import ScrollToTop from './app/layout/ScrollToTop';
 
 export const history = createBrowserHistory({window});
 
@@ -19,6 +20,7 @@ root.render(
   <React.StrictMode>
     <StoreContext.Provider value={store}>
       <HistoryRouter history={history}>
+        <ScrollToTop />
         <App />
       </HistoryRouter>
     </StoreContext.Provider>
